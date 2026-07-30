4e36119898a1a22c576c7ceed9f13c844ee5de13
Julianquispe122333 <Jhulmervalenzuela2@gmail.com>
2026-07-30 18:47:18 -0500

    revert: remover provideZoneChangeDetection que causaba pantalla blanca

--- appbiblioteca-unamba/src/app/app.config.ts
+++ appbiblioteca-unamba/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -29,7 +29,6 @@ const MyCustomPreset = definePreset(Aura, {
 
 export const appConfig: ApplicationConfig = {
 	providers: [
-		provideZoneChangeDetection({ eventCoalescing: true }),
 		provideBrowserGlobalErrorListeners(),
 		provideRouter(routes),
 		provideHttpClient(),
